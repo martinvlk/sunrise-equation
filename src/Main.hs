33fde62c22bab6fdfdfbf06842ff0006ee556f71
Martin Vlk <martin@vlkk.cz>
2015-06-03 15:09:56 +0200

    Finished the program, it now essentially works as expected. It might be made more accurate and more options could be added, but for now I leave it at this.

--- src/Main.hs
+++ src/Main.hs
@@ -8,15 +8,22 @@ https://en.wikipedia.org/wiki/Julian_day#Converting_Julian_or_Gregorian_calendar
 module Main where
 
 import qualified Data.Time as DT
+import qualified Debug.Trace as T
+import Text.Printf
 
 
+-- places
 newtype Latitude = Latitude Double deriving (Eq, Show, Ord)
 newtype Longitude = Longitude Double deriving (Eq, Show, Ord)
 type Location = (Longitude, Latitude)
 
-polipsy4 = (Longitude (-15.0591222), Latitude 49.7859756)
+polipsy4 = (Longitude 15.0591222, Latitude 49.7859756)
+aš = (Longitude 12.1949917, Latitude 50.2238694)
+praha = (Longitude 14.4341414, Latitude 50.0835494)
+place = polipsy4
 
-julianJan1st2000 = 2451545 -- the Julian day of January 1, 2000
+-- constants
+julianJan1st2000 = 2451545::Double
 sunTransitTime = 0.0009
 eccentricityTransitVariation = 0.0053
 obliguityTransitVariation = 0.0069
@@ -26,31 +33,35 @@ earthMeanAnomaly = 357.5291
 earthMeanAnomalyDegPerDay = 0.98560028
 eqOfCenterCoeficcient1 = 1.9148
 eqOfCenterCoeficcient2 = 0.0200
-eqOfCenterCoeficcient3 = 00.0003
+eqOfCenterCoeficcient3 = 0.0003
 earthPerihelion = 102.9372
 earthObliquityOfEcliptic = 23.45
-solarDiscDiameteronEarth = 0.83
+solarDiscDiameterOnEarth = 0.83
 
 main = do
   now <- DT.getCurrentTime
-  let (raise, set, dayLen) = calculateSun polipsy4 now
+  let (raise, set, dayLen) = calculateSun place now
+  raiseZoned <- DT.utcToLocalZonedTime raise
+  setZoned <- DT.utcToLocalZonedTime set
   putStrLn $ "Sun raises today at " ++ (format raise)
                ++ " and sets at " ++ (format set) ++ ". The day is "
-               ++ show dayLen ++ " hours long."
+               ++ printf "%.2f" dayLen ++ " hours long."
     where format = DT.formatTime DT.defaultTimeLocale "%H:%M"
 
 calculateSun :: Location -> DT.UTCTime -> (DT.UTCTime, DT.UTCTime, Double)
-calculateSun loc@(lon, _) now = (raise, fromJulianDays set, dayHours)
+calculateSun loc@(lon, _) now = (raiseT, setT, dayHours)
   where
-    raise           = fromJulianDays $ transit - (set - transit)
+    raise           = transit - (set - transit)
+    raiseT          = fromJulianDays (raise - 0.5)
     set             = solarNoonApprox
-                      + (eccentricityTransitVariation * sin m)
-                      - (obliguityTransitVariation * sin (2 * sel))
+                      + (eccentricityTransitVariation * (sin.rad) m)
+                      - (obliguityTransitVariation * (sin.rad) (2 * sel))
+    setT            = fromJulianDays (set - 0.5)
     solarNoonApprox = julianDateOfSolarNoon'' day loc
     m               = meanSolarAnomaly day lon
     sel             = sunEclipticalLongitude day lon
     transit         = julianDateOfSolarNoon day lon
-    dayHours        = undefined
+    dayHours        = realToFrac (DT.diffUTCTime setT raiseT) / 60 / 60
     day             = DT.utctDay now
 
 -- Julian date
@@ -66,10 +77,34 @@ toJulianDays day = toInteger d
         yr = y + 4800 - toInteger a
         a = floor $ (14 - fromIntegral m) / 12
         mo = fromIntegral $ m + 12 * a - 3
-        dy = fromIntegral y
+        dy = fromIntegral yr
 
 fromJulianDays :: Double -> DT.UTCTime
-fromJulianDays jdays = undefined
+fromJulianDays jdn = DT.UTCTime (DT.fromGregorian year
+                                      (fromInteger month) (fromInteger day))
+                                (DT.secondsToDiffTime daySeconds)
+  where
+    y = 4716
+    j = 1401
+    m = 2
+    n = 12
+    r = 4
+    p = 1461
+    v = 3
+    u = 5
+    s = 153
+    w = 2
+    b = 274277
+    c = -38
+    f = jdnIntegral + j + (((4 * jdnIntegral + b) `div` 146097) * 3) `div` 4 + c
+    jdnIntegral = floor jdn
+    e = r * f + v
+    g = (e `mod` p) `div` r
+    h = u * g + w
+    day = (h `mod` s) `div` u + 1
+    month = ((h `div` s + m) `mod` n) + 1
+    year = (e `div` p) - y + (n + m - month) `div` n
+    daySeconds = floor $ (jdn - fromIntegral jdnIntegral) * (24*60*60)
 
 -- Julian cycle since Jan 1, 2000
 julianCycle :: DT.Day -> Longitude -> Integer
@@ -85,24 +120,30 @@ julianDateOfSolarNoon' now lon@(Longitude lw) =
 
 -- This will get a very close value to the actual mean solar anomaly
 meanSolarAnomaly :: DT.Day -> Longitude -> Double
-meanSolarAnomaly now lon = trim360
+meanSolarAnomaly now lon = mod360
                            (earthMeanAnomaly + earthMeanAnomalyDegPerDay
                            * (julianDateOfSolarNoon' now lon
                            - julianJan1st2000))
 
-trim360 :: Double -> Double
-trim360 v | v < fullCircle = v
-          | otherwise      = trim360 (v - fullCircle)
+mod360 :: Double -> Double
+mod360 v | v < fullCircle = v
+          | otherwise      = mod360 (v - fullCircle)
+
+rad :: Double -> Double
+rad x = x * pi / halfCircle
+
+unrad :: Double -> Double
+unrad x = x / pi * halfCircle
 
 equationOfCenter :: DT.Day -> Longitude -> Double
-equationOfCenter now lon = (eqOfCenterCoeficcient1 * sin m)
-                         + (eqOfCenterCoeficcient2 * sin (2 * m))
-                         + (eqOfCenterCoeficcient3 * sin (3 * m))
+equationOfCenter now lon = (eqOfCenterCoeficcient1 * (sin.rad) m)
+                         + (eqOfCenterCoeficcient2 * (sin.rad) (2 * m))
+                         + (eqOfCenterCoeficcient3 * (sin.rad) (3 * m))
   where m =  meanSolarAnomaly now lon
 
 -- ecliptical longitude of the sun.
 sunEclipticalLongitude :: DT.Day -> Longitude -> Double
-sunEclipticalLongitude now lon = trim360 (meanSolarAnomaly now lon
+sunEclipticalLongitude now lon = mod360 (meanSolarAnomaly now lon
                                         + earthPerihelion
                                         + equationOfCenter now lon
                                         + halfCircle)
@@ -110,21 +151,20 @@ sunEclipticalLongitude now lon = trim360 (meanSolarAnomaly now lon
 -- accurate Julian date for solar noon
 julianDateOfSolarNoon :: DT.Day -> Longitude -> Double
 julianDateOfSolarNoon now lon = julianDateOfSolarNoon' now lon
-    + (eccentricityTransitVariation * sin (meanSolarAnomaly now lon))
-    - (obliguityTransitVariation * sin(2 * sunEclipticalLongitude now lon)) 
+    + (eccentricityTransitVariation * (sin.rad) (meanSolarAnomaly now lon))
+    - (obliguityTransitVariation * (sin.rad) (2 * sunEclipticalLongitude now lon)) 
 
 -- 
 sunDeclination :: DT.Day -> Longitude -> Double
-sunDeclination now lon = asin (sin (sunEclipticalLongitude now lon)
-                                    * sin earthObliquityOfEcliptic)
+sunDeclination now lon = (unrad.asin) ((sin.rad) (sunEclipticalLongitude now lon)
+                                    * (sin.rad) earthObliquityOfEcliptic)
 
 -- 
 hourAngle :: DT.Day -> Location -> Double
-hourAngle now (lon, (Latitude ln)) = acos
-                      ((sin (-solarDiscDiameteronEarth) - sin ln * sin decl)
-                       / (cos ln * cos decl))
+hourAngle now (lon, (Latitude ln)) = (unrad.acos.rad)
+                      (((sin.rad) (-solarDiscDiameterOnEarth) - (sin.rad) ln * (sin.rad) decl)
+                       / rad ((cos.rad) ln * (cos.rad) decl))
   where decl = sunDeclination now lon
-
 
 -- go back through the approximation again, this time we use H in the calculation
 julianDateOfSolarNoon'' :: DT.Day -> Location -> Double
